12b2260f7c4e087d6e386fe4c75fbffbdb87cc24
Zait06 <zait-hdez@hotmail.com>
2020-01-30 07:14:11 -0600

    Practica 1, 1ra parte

--- Practica1/1raParte/index.py
+++ Practica1/1raParte/index.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 import numpy as np
 import algoritmos00 as alg
 
@@ -6,7 +7,9 @@ if __name__== "__main__":
     nombre=sys.argv[1]
     ff=np.loadtxt("./numeros/"+nombre+"n.txt",dtype=int)
     print("Trabajando en el ordenamiento, espere por favor...\n")
-    ord00=alg.insertSort(ff)
-    ord01=alg.insertSort(ff)
+    
+    ord00=alg.insertSort(copy.copy(ff))
+    ord01=alg.insertSort(copy.copy(ff))
+
     print("Tiempo de caso promedio de Insert Sort:\t {:2.10f} seg.".format(ord00[1]))
     print("Tiempo de caso promeido de Bubble Sort:\t {:2.10f} seg.".format(ord01[1]))

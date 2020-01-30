131cdcd9e89699b054db339ab41bcf46028b838b
Zait06 <zait-hdez@hotmail.com>
2020-01-30 12:57:34 -0600

    Programa principal

--- Practica1/1raParte/index.py
+++ Practica1/1raParte/index.py
@@ -12,28 +12,33 @@ if __name__== "__main__":
 
     i=1000;j=0
     while i<=10000:
-        fm=np.loadtxt("./numeros/"+str(i)+"m.txt",dtype=int)            # Lee los datos de un txt
-        fn=np.loadtxt("./numeros/"+str(i)+"n.txt",dtype=int)            # Lee los datos de un txt
-        fp=np.loadtxt("./numeros/"+str(i)+"p.txt",dtype=int)            # Lee los datos de un txt
-        # dataIS[j][0]=i
-        # ord00,dataIS[j][1]=alg.insertSort(copy.copy(fm))           # Se hace copia del arreglo y se ordena
-        # ord01,dataIS[j][2]=alg.insertSort(copy.copy(fn))           # Se hace copia del arreglo y se ordena
-        # ord02,dataIS[j][3]=alg.insertSort(copy.copy(fp))           # Se hace copia del arreglo y se ordena
-        # print("Listo {}".format(i))
+        # Lee los datos de un txt dependiendo el orden
+        fm=np.loadtxt("./numeros/"+str(i)+"m.txt",dtype=int)            # Mejor caso
+        fn=np.loadtxt("./numeros/"+str(i)+"n.txt",dtype=int)            # Caso promedio
+        fp=np.loadtxt("./numeros/"+str(i)+"p.txt",dtype=int)            # Peor caso
+        
+        # Se hace copia del arreglo y se ordena por el metodo insert sort
+        print("\n\tNumero de datos:\t{:3d}".format(i))
+        dataIS[j][0]=i
+        ord00,dataIS[j][1]=alg.insertSort(copy.copy(fm))            # Mejor caso
+        ord01,dataIS[j][2]=alg.insertSort(copy.copy(fn))            # Caso promedio
+        ord02,dataIS[j][3]=alg.insertSort(copy.copy(fp))            # Peor caso
+        print("Tiempo del mejor caso de Insert Sort:\t {:2.10f} seg.".format(dataIS[j][1]))
+        print("Tiempo de caso promedio de Insert Sort:\t {:2.10f} seg.".format(dataIS[j][2]))
+        print("Tiempo del peor caso de Insert Sort:\t {:2.10f} seg.".format(dataIS[j][3]))
+        
+        # Se hace copia del arreglo y se ordena por el metodo insert sort
         dataBS[j][0]=i
-        ord00,dataBS[j][1]=alg.bubbleSort(copy.copy(fm))           # Se hace copia del arreglo y se ordena
-        ord01,dataBS[j][2]=alg.bubbleSort(copy.copy(fn))           # Se hace copia del arreglo y se ordena
-        ord02,dataBS[j][3]=alg.bubbleSort(copy.copy(fp))           # Se hace copia del arreglo y se ordena
-        print("Listo {}".format(i))
+        ord00,dataBS[j][1]=alg.bubbleSort(copy.copy(fm))           # Mejor caso
+        ord01,dataBS[j][2]=alg.bubbleSort(copy.copy(fn))           # Caso promedio
+        ord02,dataBS[j][3]=alg.bubbleSort(copy.copy(fp))           # Peor caso
+        print("Tiempo del mejor caso de Bubble Sort:\t {:2.10f} seg.".format(dataBS[j][1]))
+        print("Tiempo de caso promedio de Bubble Sort:\t {:2.10f} seg.".format(dataBS[j][2]))
+        print("Tiempo del peor caso de Bubble Sort:\t {:2.10f} seg.".format(dataBS[j][3]))
+
         i=i+1000
         j=j+1
 
-    # np.savetxt("InsertTime.txt",dataIS,fmt='%f')
+    np.savetxt("InsertTime.txt",dataIS,fmt='%f')
     np.savetxt("BubbleTime.txt",dataBS,fmt='%f')
     print("\nTrabajo terminado\n")
-
-    # ord00=alg.insertSort(copy.copy(ff))
-    # print("Tiempo de caso promedio de Insert Sort:\t {:2.10f} seg.".format(ord00[1]))
-    
-    # ord01=alg.bubbleSort(copy.copy(ff))
-    # print("Tiempo de caso promeido de Bubble Sort:\t {:2.10f} seg.".format(ord01[1]))

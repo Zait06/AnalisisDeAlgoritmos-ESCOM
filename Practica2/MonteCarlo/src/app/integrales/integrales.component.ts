0d6456e893c820dac7d8c09f6ff85ee6c7e84324
Zait06 <zait-hdez@hotmail.com>
2020-02-20 12:57:51 -0600

    Integrales

--- Practica2/MonteCarlo/src/app/integrales/integrales.component.ts
+++ Practica2/MonteCarlo/src/app/integrales/integrales.component.ts
@@ -2,16 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { NumberSymbol } from '@angular/common';
 
 class Integrales{
-  a:string;
+  a:number;
   b:number;
   n:number;
-  indice:boolean;
+  suma:number;
+  indice:number;
+  resultado:number;
+  datos:Array<number>
 
   constructor(){
     this.a=this.a;
     this.b=this.b;
     this.n=this.n;
-    this.indice=this.indice;
+    this.suma=0
+    this.resultado=0
+  }
+
+  funcion1(x:number){
+    return Math.pow((1-Math.pow(x,2)),3)
+  }
+
+  aproxFun1(i:number=0,s:number=0,ui:number=0,ent:number=0){
+    s=0; ent=0; this.suma=0;
+    for(i=0;i<this.n;i++){
+      ui=Math.random()*(1-0)+0
+      ent=this.funcion1((ui*(this.b-this.a) + this.a));
+      //this.datos.push(ent)
+      this.suma+=ent;
+    }
+    this.resultado=((this.b-this.a)/this.n)*this.suma
   }
 }
 
@@ -31,7 +50,8 @@ export class IntegralesComponent implements OnInit {
   }
 
   elegirFun(){
-    console.log(this.funcionIntegral.indice);
+    this.funcionIntegral.aproxFun1()
+    console.log(this.funcionIntegral.resultado)
   }
 
 }

d28588a5af8ad0adb4baf55ecc4d7d832f1f6b23
Zait06 <zait-hdez@hotmail.com>
2020-05-27 19:18:35 -0500

    LCS

--- Practica05/main.cpp
+++ Practica05/main.cpp
@@ -1,38 +1,41 @@
 #include "LCS.h"
+#include <string>
 #include <fstream>
 
 int main(int argc, char *argv[]){
-    // if(argc!=2){
-    //     cout<<"Forma de uso: "<<argv[0]<<" nombre_archivo.txt"<<endl;
-    //     exit(0);
-    // }
+    if(argc!=2){
+        cout<<"Forma de uso: "<<argv[0]<<" nombre_archivo.txt"<<endl;
+        exit(0);
+    }
     
-    // int opc=atoi(argv[1]);      // Numero de matrices
+    int a,b;
+    string s;                   // Variable auxiliar para guardar la variable
+    char *aa, *bb;              // Apuntadores a las cadenas a analizar
+    ifstream in(argv[1]);       // Abrimos un documento con el nombre que tenga argv[1]                 
+    /*
+        Se lee el documento linea por linea. Dicha linea se guarda en la variable
+        's'.
+    */
+    getline(in,s);              // Se lee la primera linea
+    a=s.size();                 // Se toma el tamaño de la primera cadena
+    aa=new char[a];             // Se crea un arreglo dinámico char de tamanio a
+    strcpy(aa,s.c_str());       // Se copia la primera cadena en el arreglo
+    cout<<aa<<endl;
 
-    // ifstream in(argv[1]);       // Abrimos un documento con el nombre que tenga argv[1]
-    // string s;                   // Variable auxiliar para guardar la variable
-    // /*
-    //     Se lee el documento linea por linea. Dicha linea se guarda en la variable
-    //     's'.
-    // */
-    // while(getline(in,s)){
-    //     cout<<s<<endl;
-    // }
-    // in.close();
-    string c00="AMAPOLA";
-    string c01="MATAMOSCAS";
-    int a=c00.size();
-    int b=c01.size();
-    
-    char *aa, *bb;
-    aa=new char[a];
-    bb=new char[b];
-    strcpy(aa,c00.c_str());
-    strcpy(bb,c01.c_str());
+    getline(in,s);              // Lectura de linea vacia
+    cout<<endl;
 
-    LCS lcs00(aa,bb,a,b);
-    lcs00.runAlgorithm();
-    lcs00.printTableLength();
+    getline(in,s);              // Se lee la tercera linea
+    b=s.size();                 // Se toma el tamaño de la segunda cadena
+    bb=new char[b];             // Se crea un arreglo dinámico char de tamanio b
+    strcpy(bb,s.c_str());       // Se copia la segunda cadena en el arreglo
+    cout<<bb<<endl;
+
+    in.close();                 // Se cierra el documento
+
+    LCS lcs00(aa,bb,a,b);       // Se crea un objeto de la clase LCS
+    lcs00.runAlgorithm();       // Genera el algoritmo
+    lcs00.printTableLength();   // Imprime la tabla de valores 
 
     return 0;
 }
